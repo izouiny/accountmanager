a6c0b1f4b0373033382100316f434c437c2bf603
izouiny <ilias.zouine@umontreal.ca>
2023-12-26 20:31:15 -0500

    2

--- CLI.java
+++ CLI.java
@@ -32,6 +32,11 @@ public class CLI {
     }
 
     private void signUp() {
+        System.out.println("1. Buyer\n2. Seller");
+        System.out.print("Choose type: ");
+        int type = scanner.nextInt();
+        scanner.nextLine(); // consume newline
+    
         System.out.print("Enter name: ");
         String name = scanner.nextLine();
         System.out.print("Enter email: ");
@@ -40,12 +45,7 @@ public class CLI {
         String phone = scanner.nextLine();
         System.out.print("Enter password: ");
         String password = scanner.nextLine();
-
-        System.out.println("1. Buyer\n2. Seller");
-        System.out.print("Choose type: ");
-        int type = scanner.nextInt();
-        scanner.nextLine(); // consume newline
-
+    
         if (type == 1) {
             System.out.print("Enter first name: ");
             String firstName = scanner.nextLine();
@@ -53,37 +53,43 @@ public class CLI {
             String username = scanner.nextLine();
             System.out.print("Enter shipping address: ");
             String shippingAddress = scanner.nextLine();
-
+    
             Buyer buyer = new Buyer(name, firstName, username, email, phone, shippingAddress, password);
             userManager.addUser(buyer);
             System.out.println("Buyer account created successfully.");
         } else if (type == 2) {
             System.out.print("Enter address: ");
             String address = scanner.nextLine();
-
+    
             Seller seller = new Seller(name, email, phone, address, password);
             userManager.addUser(seller);
             System.out.println("Seller account created successfully.");
         } else {
             System.out.println("Invalid type.");
         }
-    }
+    }    
 
     private void logIn() {
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter password: ");
-        String password = scanner.nextLine();
-    
         System.out.println("1. Buyer\n2. Seller");
         System.out.print("Choose type: ");
         int type = scanner.nextInt();
         scanner.nextLine(); // consume newline
-    
+
+        String identifier;
+        if (type == 1) {
+            System.out.print("Enter username: ");
+            identifier = scanner.nextLine();
+        } else {
+            System.out.print("Enter name: ");
+            identifier = scanner.nextLine();
+        }
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
         String userType = (type == 1) ? "Buyer" : "Seller";
-        if (userManager.checkUser(name, password, userType)) {
+        if (userManager.checkUser(identifier, password, userType)) {
             System.out.println("Logged in successfully as " + userType);
-            // Vous pouvez ajouter ici la logique pour les actions après la connexion réussie
         } else {
             System.out.println("Incorrect username or password. Please try again.");
             start(); // Retour au menu principal
